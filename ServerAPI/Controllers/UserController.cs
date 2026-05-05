43959aef669bf826111f4aa34cd3aea75b96475e
sebkahr <sebkahr@gmail.com>
2026-05-05 09:53:45 +0200

    tilføjet kode til UserController (dependency injection) og tilføjet kode til login page

--- ServerAPI/Controllers/UserController.cs
+++ ServerAPI/Controllers/UserController.cs
@@ -6,5 +6,22 @@ namespace ServerAPI.Controllers;
 
 public class UserController
 {
-    
+    private IUserRepository FakeUserRepository;
+
+    public UserController(IUserRepository userRepository)
+    {
+        this.FakeUserRepository = userRepository;
+    }
+
+    [HttpGet]
+    public User[] GetAll()
+    {
+        return FakeUserRepository.GetAll();
+    }
+
+    [HttpPost]
+    public User? Validate(string email, string password)
+    {
+        return FakeUserRepository.Validate(email, password);
+    }
 }
