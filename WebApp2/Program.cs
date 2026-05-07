420e62d05a07e4dcf39d5d66eac85252e91ac2a3
luwam09habte <luwam1@hotmail.dk>
2026-05-07 11:50:52 +0200

    Virker stadig ikke:( kan ikke koble besvarelserne ...

--- WebApp2/Program.cs
+++ WebApp2/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,7 +7,8 @@ using WebApp2;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredSessionStorage();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5160/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5160") });
 
 await builder.Build().RunAsync();
