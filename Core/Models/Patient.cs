688aa0da1fab35f4e3f85be7c14869011d3ca382
luwam09habte <luwam1@hotmail.dk>
2026-05-18 11:38:08 +0200

    Opdateringer

--- Core/Models/Patient.cs
+++ Core/Models/Patient.cs
@@ -9,6 +9,8 @@ public class Patient
     public string Role { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
     public string InjuryType { get; set; } = string.Empty;
+    public DateTime InjuryDate { get; set; }
+
     public bool IsActive { get; set; } = true;
     
     /*public enum PatientPhase { get; set; }*/
