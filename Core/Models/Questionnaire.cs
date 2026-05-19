9a962ba22faf180d2ac4eb7ed12895ab59bdb8ca
sebkahr <sebkahr@gmail.com>
2026-05-19 10:36:38 +0200

    ændrede ting

--- Core/Models/Questionnaire.cs
+++ Core/Models/Questionnaire.cs
@@ -12,7 +12,7 @@ public class Questionnaire
 
     public List<Question> Questions { get; set; } = new();
 
-    public int PatientId { get; set; }
+    public int AdminId { get; set; }
     /* For injurytype men bliver ikke taget fat i*/
     public List<string> InjuryTypes { get; set; } = new();
     
