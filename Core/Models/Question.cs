b1c5355dcab0cac1392c5b4caecfe156e59e4316
luwam09habte <luwam1@hotmail.dk>
2026-05-21 12:37:21 +0200

    push

--- Core/Models/Question.cs
+++ Core/Models/Question.cs
@@ -3,16 +3,24 @@ namespace Core.Models;
 public class Question
 {
     public int QuestionId { get; set; }
-
+    
+    public string Title { get; set; } = string.Empty;
     public string Text { get; set; } = string.Empty;
 
     public List<string> Option { get; set; } = new();
     
     public int QuestionnaireId { get; set; }
-    
-    /* Så spørgsmål svar er krævet*/
-    public bool IsRequired { get; set; } 
-    
-    /* For at vælge flere valgmuligheder?*/
-    public string Type { get; set; } = "MultipleChoice";
+
+    public List<TextField> TextFields { get; set; } = new();
+
+    public bool IsRequired { get; set; }
+
+    public string Type { get; set; } = "Scale5";
+}
+
+public class TextField
+{
+    public string Label { get; set; } = string.Empty;
+
+    public string Placeholder { get; set; } = string.Empty;
 }
--- Core/Models/Questionnaire.cs
+++ Core/Models/Questionnaire.cs
@@ -6,16 +6,12 @@ public class Questionnaire
 {
     [BsonId]
     public int QuestionnaireId { get; set; }
-
     public string Title { get; set; } = string.Empty;
-
     public string Text { get; set; } = string.Empty;
-
-    public List<Question> Questions { get; set; } = new();
-
     public int AdminId { get; set; }
-    /* For injurytype men bliver ikke taget fat i*/
     public List<string> InjuryTypes { get; set; } = new();
+    public List<Question> Questions { get; set; } = new();
+    public List<int> AssignedToPatientIds { get; set; } = new();
     
     /*Så det kan tildelses på patientID
     public List<int> AssignedToPatientIds { get; set; } = new();*/
