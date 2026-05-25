f935a2395d1a1df4fd9afbc8e09f0bc5e2057a2e
luwam09habte <luwam1@hotmail.dk>
2026-05-25 11:20:18 +0200

    eksport knap

--- ServerAPI/Controllers/QuestionnaireAnswerController.cs
+++ ServerAPI/Controllers/QuestionnaireAnswerController.cs
@@ -1,6 +1,7 @@
 using Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using ServerAPI.Interfaces;
+using ClosedXML.Excel;
 
 namespace ServerAPI.Controllers;
 
@@ -75,5 +76,48 @@ public class QuestionnaireAnswerController : ControllerBase
         var patientAnswers = _repository.GetByPatient(answer.PatientId);
 
         return Ok(patientAnswers);
+    }
+    
+    [HttpGet("export")]
+    public IActionResult ExportToExcel()
+    {
+        var answers = _repository.GetAll();
+
+        using var workbook = new XLWorkbook();
+
+        var worksheet = workbook.Worksheets.Add("Besvarelser");
+
+        worksheet.Cell(1, 1).Value = "PatientId";
+        worksheet.Cell(1, 2).Value = "QuestionnaireId";
+        worksheet.Cell(1, 3).Value = "QuestionId";
+        worksheet.Cell(1, 4).Value = "Svar";
+        worksheet.Cell(1, 5).Value = "Dato";
+
+        int row = 2;
+
+        foreach (var answer in answers)
+        {
+            foreach (var q in answer.Answers)
+            {
+                worksheet.Cell(row, 1).Value = answer.PatientId;
+                worksheet.Cell(row, 2).Value = answer.QuestionnaireId;
+                worksheet.Cell(row, 3).Value = q.QuestionId;
+                worksheet.Cell(row, 4).Value = q.SelectedOption;
+                worksheet.Cell(row, 5).Value = answer.SubmittedAt;
+
+                row++;
+            }
+        }
+
+        using var stream = new MemoryStream();
+
+        workbook.SaveAs(stream);
+
+        var content = stream.ToArray();
+
+        return File(
+            content,
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "Besvarelser.xlsx");
     }
 }
