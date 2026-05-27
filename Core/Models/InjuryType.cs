37951d52d8a43cc67458dc8702deb40532c0d2e7
luwam09habte <luwam1@hotmail.dk>
2026-05-27 12:09:09 +0200

    Til Render

--- Core/Models/InjuryType.cs
+++
@@ -1,8 +0,0 @@
-namespace Core.Models;
-
-public class InjuryType 
-{ 
-    public int InjuryTypeId { get; set; }
-    
-    public string Name { get; set; } = string.Empty;
-}
--- Core/Models/QuestionnaireDelivery.cs
+++
@@ -1,9 +0,0 @@
-/*Bruges ikke*/ namespace Core.Models;
-
-public class QuestionnaireDelivery
-{
-    public int id { get; set; }
-    public int questionnaireId { get; set; }
-    public int patientId { get; set; }
-   
-}
